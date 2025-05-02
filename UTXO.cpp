e16a3514f72d97f2cd6c2dfebbeefdea95402204
DrinoSan <s.becirbegovic@gmail.com>
2025-05-02 22:59:13 +0200

    Made UTXO jsonify

--- UTXO.cpp
+++ UTXO.cpp
@@ -10,5 +10,13 @@ void to_json( json& j, const UTXO& u )
    j[ "amount" ]      = u.amount;
    j[ "address" ]     = u.address;
 }
-};
 
+// ----------------------------------------------------------------------------
+void from_json( const json& j, UTXO& u )
+{
+   j.at( "txid" ).get_to( u.txid );
+   j.at( "outputIndex" ).get_to( u.outputIndex );
+   j.at( "amount" ).get_to( u.amount );
+   j.at( "address" ).get_to( u.address );
+}
+};   // namespace utxo
